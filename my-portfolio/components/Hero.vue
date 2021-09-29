c4767540a0f84ad096a3f8ec76b2d31036e13f9d
Paris Charalampidis <curlycode@outlook.com>
2021-09-29 13:58:34 +0300

    added new Partial and set notification functionality

--- my-portfolio/components/Hero.vue
+++ my-portfolio/components/Hero.vue
@@ -1,6 +1,11 @@
 <template>
     
         <div class="container mx-auto">
+            <div class="modal">
+                <Modal v-show="showModal"  />
+                
+            </div>
+
             <div class="hero-image ">
                 <div class="hero-text text-white text-center ">
                     <h1 class="font-bold text-2xl">Paris Charalampidis</h1>
@@ -13,23 +18,49 @@
                                  <p>parou.char@gmail.com</p>
                         </div>
                     <div class="mt-6 ml-15">
-                    <a class="button font-semibold bg-gray-600 rounded-2xl p-2  hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"  href="resume.pdf" download>Download Resume</a>
+                    <a id="hero-modal" class="button font-semibold bg-gray-600 rounded-2xl p-2  hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50"  @click="setNotification" >Download Resume</a>
                     </div>
+                  
                 </div>
+            
+            
             </div>    
         </div>
     
 </template>
+<script>
+ import Modal from "~/partial/Modal.vue"
+    export default{
+        name: "Hero",
+        components:{
+         Modal
+        },
+        data(){  
+            return{
+                showModal: false,
+            }
+        },
+        methods:{
+            setNotification(){
+                setTimeout(() =>{
+                    this.showModal = true;
+                },1000);
+            }
+        },
+    }
+</script>
 <style scoped>
-   
+#hero-modal{
+    visibility: block;
+}
 .hero-text{
   position: absolute;
   top:50%;
   left:50%;
   transform: translate(-50%, -50%);
 }
-
 .hero-image{
+   
     background-image:linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)),
     url("~/static/cover-photo.jpg");
     height:300px;
---
+++ my-portfolio/partial/Modal.vue
@@ -0,0 +1,46 @@
+<template>
+    <div class="container mx-auto">
+         <div class="modal-overlay">
+             <div class="modal bg-red-500">     
+                    <div>
+                         <h1 class="text-base font-semibold text-white">Thank you for downloading my resume!</h1>
+                         <a href="/">x</a>
+                    </div>    
+            </div>
+        </div>
+    </div>
+</template> 
+<script>
+    export default{
+        
+    }
+</script>
+<style scoped>
+.modal-overlay {
+margin-bottom:25px;
+  display: flex;
+  justify-content: center;
+  padding:10px;
+  
+}
+.modal-overlay:target{
+  visibility: hidden;
+}
+
+.modal {
+  position:relative;
+  text-align: center; 
+  height: 100px;
+  width: 400px;
+  border-radius:8px;
+  margin-top:30px;
+  padding:1em;
+}
+a{ 
+  color:white;
+  position:absolute;
+  top:15px;
+  right:10px;
+}
+
+</style>
